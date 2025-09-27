c1e3e59b000f66c116cbece7589b77a120607d50
oldkaseb <hamedsocialworks@gmail.com>
2025-09-27 09:14:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from aiogram.filters import Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardmarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from datetime import datetime, timedelta
 import re
 
